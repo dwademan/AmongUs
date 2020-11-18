68ad6e01021f65a5c4e46ed8fd63bc3067871ec0
David Wademan <david_wademan@bloomfield.edu>
2020-11-18 21:48:34 +0000

    Nov11updates

--- amongus.cs
+++ amongus.cs
@@ -8,89 +8,119 @@ namespace AmongUs
         private PlayerColors _playerColor;
         private Rooms _currentRoom;
         private int _roomNumber;
-        private int _roomHeight = 49;
-        private int _roomWidth = 49;
-        private int x = 3;
+        private int _roomBlockSize = 12;
+        private int _roomSize;
+        private int _roomColumn1;
+        private int _roomColumn2;
+        private int _statusBlock;
 
         public void StartGame() 
         {
             Console.ResetColor();
             Console.Clear();
+            DrawRooms();
 
             GetPlayerColor();
-            Console.SetCursorPosition(2, 2);
-            Console.ForegroundColor = (ConsoleColor)_playerColor;
-            Console.WriteLine("Start " + _playerColor);
-            Console.ResetColor();
+            DisplayPlayerStatus();
+            ShowPlayerColors();
 
             _currentRoom = Rooms.cafeteria;
+            //_currentRoom = Rooms.upperengines;
             GetRoomInfo();
-
-            _currentRoom = Rooms.admin;
-            GetRoomInfo();
-
-            DrawRooms();
-            //Colors();
             
         }
 
-        public void Colors()
+        public void DisplayPlayerStatus()
         {
-           Console.Clear();
+            Console.SetCursorPosition(_statusBlock+2, 3);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("Player Color: ");
+            Console.ForegroundColor = (ConsoleColor)_playerColor;
+            Console.Write(" " + _playerColor);
+        }
 
+        public void ShowPlayerColors()
+        {
+           int x = 0; 
            foreach (PlayerColors color in Enum.GetValues(typeof(PlayerColors)))
-           {              
-              Console.ForegroundColor = (ConsoleColor)color;
-              Console.WriteLine(color);
+           {
+               Console.SetCursorPosition(_statusBlock+2, 5+x);               
+               Console.ForegroundColor = (ConsoleColor)color;
+               Console.WriteLine(color);
+               x += 1;
            }
-
         }
 
         public void DrawRooms()
-        {            
+        {
+            Console.Clear();            
             Console.CursorVisible = false;
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.SetWindowSize(_roomWidth, _roomHeight);
+            _roomSize = (_roomBlockSize * 3) + 4;
+            _roomColumn1 = ((_roomSize-4)/3) + 2;
+            _roomColumn2 = (_roomColumn1*2) - 1;
+            Console.SetWindowSize(_roomColumn1, _roomColumn2);
             BuildWall();
         }
 
         public void BuildWall()
         {            
-            for (int i = 0; i < _roomWidth; i++)
+            for (int i = 1; i < _roomSize+1; i++)
             {
                 Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
 
                 // Draw outter boxes
-                Console.SetCursorPosition(i, 0);
+                Console.SetCursorPosition(i, 1);
                 Console.Write("#");
-                Console.SetCursorPosition(0, i);
+                Console.SetCursorPosition(1, i);
                 Console.Write("#");
-                Console.SetCursorPosition(_roomWidth, i);
+                Console.SetCursorPosition(_roomSize, i);
                 Console.Write("#");
-                Console.SetCursorPosition(i, _roomHeight);
+                Console.SetCursorPosition(i, _roomSize);
                 Console.Write("#");   
 
                 // Draw inner boxes
-                Console.SetCursorPosition(16, i);
+                Console.SetCursorPosition(_roomColumn1, i);
                 Console.Write("#");
-                Console.SetCursorPosition(i, 16);
+                Console.SetCursorPosition(i, _roomColumn1);
                 Console.Write("#");            
-                Console.SetCursorPosition(32, i);
+                Console.SetCursorPosition(_roomColumn2, i);
                 Console.Write("#");
-                Console.SetCursorPosition(i, 32);
+                Console.SetCursorPosition(i, _roomColumn2);
                 Console.Write("#");                             
             }
-
-            for (int i = 0; i < _roomWidth+1; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                Console.Write(i);
-                Console.SetCursorPosition(i, 0);
-                Console.Write(i);                
-            }
-
+  
             Console.WriteLine("");
+            BuildStatusBlock();
+        }
+
+        public void BuildStatusBlock()
+        {            
+           // Draw status block top and bottom border
+           _statusBlock = _roomSize + 10;
+           Console.ForegroundColor = ConsoleColor.White;
+           Console.BackgroundColor = ConsoleColor.Black;
+
+           for (int i = 1; i < _statusBlock+1; i++)
+           {
+               Console.SetCursorPosition(i+_statusBlock, 1);
+               Console.Write("#");
+               Console.SetCursorPosition(i+_statusBlock, 12);
+               Console.Write("#");
+           }
+        
+           // Draw status block left and right border
+           for (int i = 1; i < 13; i++)
+           {
+               Console.SetCursorPosition(_statusBlock, i);
+               Console.Write("#");
+               Console.SetCursorPosition(_statusBlock*2, i);
+               Console.Write("#");
+           }
+           
         }
 
         public void GetPlayerColor()
@@ -102,11 +132,11 @@ namespace AmongUs
 
         public void GetRoomInfo() 
         {
-            Console.WriteLine(" ");
-            Console.SetCursorPosition(x, x);
+            Console.SetCursorPosition(_statusBlock+2, 4);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
            _roomNumber = (int) _currentRoom;
-           Console.WriteLine("You are in " + _currentRoom + ": " + _roomNumber);
-           x +=1;
+           Console.WriteLine("Current Room: " + _currentRoom + " " + _roomNumber);
         }
     }
 
@@ -121,15 +151,15 @@ namespace AmongUs
     } 
 
     public enum Rooms {
+       upperengines,
        cafeteria,
        weapons,
-       navigation,
-       shields,
-       storage,
-       admin,
-       electrical,
        lowerengines,
-       upperengines
+       admin,
+       navigation,
+       electrical,
+       storage,
+       shields
     }
  
 }
